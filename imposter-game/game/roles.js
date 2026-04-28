02d6a944d1dc936d516b8237062c041ceb350244
prashanta <pratiksubedi2100@gmail.com>
2026-04-28 16:16:26 +0545

    Delete imposter-game directory

--- imposter-game/game/roles.js
+++
@@ -1,8 +0,0 @@
-// game/roles.js - Imposter assignment
-function assignRoles(players) {
-  const ids = players.map(p => p.id);
-  const imposterId = ids[Math.floor(Math.random() * ids.length)];
-  return { imposterId };
-}
-
-module.exports = { assignRoles };
